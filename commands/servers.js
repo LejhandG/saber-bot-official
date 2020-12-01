f2892f25e4526dae727529dcf4abf93fb28c1c0e
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2020-12-01 10:22:50 +0530

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -5,7 +5,7 @@ alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play, client) => {
   if(message.author.id !== '565483539500367884') return message.reply("You do not have the permission to use the command !");
     bot.guilds.cache.forEach(guild => {
-  message.channel.send(`${guild.name} | ${guild.id}`);
+  message.channel.send(`${guild.name} | ${guild.id} | ${guild.memberCount}`);
 })
 }
 }
