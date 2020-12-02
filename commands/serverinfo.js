f31e8698c1de9a8e6c2f9ffcd1dd4970bd3ef9a8
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2020-12-02 10:42:02 +0530

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -6,18 +6,58 @@ module.exports = {
 description: "Shows the info of the server",
 alias: ["si"],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
-  let embed = new Discord.MessageEmbed()
-            .setColor("RANDOM")
-            .setTitle("Server Info")
-            .setThumbnail(message.guild.iconURL({ dynamic: true }))
-            .setDescription(`${message.guild}'s information`)
-            .addField("Owner", `The owner of this server is ${message.guild.owner}`)
-            .addField("Member Count", `This server has ${message.guild.memberCount} members`)
-            .addField("Emoji Count", `This server has ${message.guild.emojis.cache.size} emojis`)
-            .addField("Roles Count", `This server has ${message.guild.roles.cache.size} roles`)
-            .setFooter("Command used by " + message.author.tag)
-            
-
-        message.channel.send(embed)
-}
+    let icon = message.guild.iconURL({size: 2048}); // Server Avatar
+    
+    let region = {
+      "brazil": "Brazil",
+      "eu-central": "Central Europe",
+      "singapore": "Singapore",
+      "london": "London",
+      "russia": "Russia",
+      "japan": "Japan",
+      "hongkong": "Hongkong",
+      "sydney": "Sydney",
+      "us-central": "U.S. Central",
+      "us-east": "U.S. East",
+      "us-south": "U.S. South",
+      "us-west": "U.S. West",
+      "eu-west": "Western Europe"
+    }
+    
+    // Members
+    let member = message.guild.members;
+    let total = message.guild.memberCount;
+    
+    // Channels
+    let channels = message.guild.channels;
+    let text = channels.cache.filter(r => r.type === "text").size,
+        vc = channels.cache.filter(r => r.type === "voice").size,
+        category = channels.cache.filter(r => r.type === "category").size,
+        totalchan = channels.cache.size;
+    
+    // Region
+    let location = region[message.guild.region];
+    
+    // Date
+    let x = Date.now() - message.guild.createdAt;
+    let h = Math.floor(x / 86400000) // 86400000, 5 digits-zero.
+    let dateformat = require('dateformat');
+    let created = dateformat(message.guild.createdAt); // Install "dateformat" first.
+    
+    const embed = new Discord.MessageEmbed()
+    .setColor(0x7289DA)
+    .setTimestamp(new Date())
+    .setThumbnail(icon)
+    .setAuthor(message.guild.name, icon)
+    .setDescription(`**ID:** ${message.guild.id}`)
+    .addField("Region", location)
+    .addField("Date Created", `${created} \nsince **${h}** day(s)`)
+    .addField("Owner", `**${message.guild.owner.user.tag}** \n\`${message.guild.owner.user.id}\``)
+    .addField("Member Count", `This server has ${message.guild.memberCount} members`)
+    .addField(`Channels [${totalchan}]`, `Text: ${text} \nVoice: ${vc} \nCategory: ${category}`)
+    .addField("Emoji Count", `This server has ${message.guild.emojis.cache.size} emojis`)
+    .addField("Roles Count", `This server has ${message.guild.roles.cache.size} roles`)
+    .setFooter("Command used by " + message.author.tag)
+    message.channel.send(embed); // Let's see if it's working!
+  }
 }
