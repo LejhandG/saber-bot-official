5b823020778231330291caad30e8e957bc38bba8
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2020-12-02 12:05:33 +0530

    Update call.js

--- commands/call.js
+++ commands/call.js
@@ -12,7 +12,7 @@ run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQ
 
   const helpembed = new MessageEmbed()
       .setColor("RANDOM")
-      .setImage("https://api.alexflipnote.dev/calling?text=" + args.slice(1, args.length + 1).join(" "))
+      .setImage("https://api.alexflipnote.dev/calling?text=" + args.slice(1, args.length + 1).join("+"))
       .setFooter("Command used by " + message.author.tag)
     message.channel.send(helpembed);
 }
