5f38dfc0b69e0adb7da37c65e139d65288711303
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2020-12-02 12:12:35 +0530

    Update pokemon.js

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -12,7 +12,7 @@ run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQ
   const helpembed = new MessageEmbed()
       .setColor("RANDOM")
       .setTitle("A Pokemon")
-      .setImage("https://i.some-random-api.ml/pokemon/" + args.slice(1, args.length + 1).join(" ") + ".png")
+      .setImage("https://i.some-random-api.ml/pokemon/" + args.slice(1, args.length + 1).join("+") + ".png")
       .setFooter("Command used by " + message.author.tag)
     message.channel.send(helpembed);
 }
