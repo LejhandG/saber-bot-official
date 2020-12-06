6e34cec5242bbe6ea77556f7ca695fe4ed541477
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2020-12-06 13:55:51 +0530

    Update and rename embed.js to advancedembed.js

--- commands/advancedembed.js
+++ commands/advancedembed.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require('discord.js')
 
 module.exports = {
-    name: "embed",
+    name: "advancedembed",
 description: "Embed",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
