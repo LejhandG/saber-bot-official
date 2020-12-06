9e277d31dff20b0fe5199269fc2d3672bb78a1ec
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2020-12-06 14:52:40 +0530

    Create remove.js

---
+++ commands/remove.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: "remove",
+description: "Removes a song from the queue",
+alias: [],
+run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+    if (!args[1]) return message.channel.send("**Please Enter A Song Number!**")
+
+    const { channel } = message.member.voice;
+    if (!channel) return message.channel.send('I\'m sorry but you need to be in a voice channel to remove a particular song number!');
+    if (message.guild.me.voice.channel !== message.member.voice.channel) {
+        return message.channel.send("**You Have To Be In The Same Channel With The Bot!**");
+    };
+    if (!serverQueue) return message.channel.send("Nothing playing in this server");
+  try {
+    if (args[1] < 1 && args[1] >= serverQueue.songs.length) {
+        return message.channel.send("Please Enter A Valid Song Number!");
+    }
+    serverQueue.songs.splice(args[1] - 1, 1);
+    return message.channel.send(`Removed song number ${args[1]} from queue`);
+  } catch {
+      serverQueue.connection.dispatcher.end();
+      return message.channel.send("TRY AGAIN!")
+  }
+}
+}
