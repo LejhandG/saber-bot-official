1b36b04685c7db681f201845054106d153d28a49
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2020-12-12 20:59:06 +0530

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -9,10 +9,16 @@ module.exports = {
   if (!user) user = message.author
 
   let avatar = user.displayAvatarURL({size: 4096, dynamic: true});
+  let pngavatar = user.displayAvatarURL({ format: 'png' });
+  let jpgavatar = user.displayAvatarURL({ format: 'jpg' });
 
   let embed = new MessageEmbed()
   .setAuthor(`${user.tag}`, user.displayAvatarURL({size: 4096}))
-  .setDescription(`[Avatar URL of **${user.tag}**](${avatar})`)
+  .setDescription(`
+  **❯** [Avatar URL of **${user.tag}**](${avatar})
+  **❯** [Png Avatar URL of **${user.tag} avatar **](${pngavatar})
+  **❯** [Jpg URL of **${user.tag} avatar**](${jpgavatar})
+  `)
   .setImage(user.displayAvatarURL({ dynamic: true}))
   .setColor("RANDOM")
   .setFooter("Command used by " + message.author.tag)
