31991fa82b8164a82a84c991f0be0562502443d3
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2020-12-15 16:01:47 +0530

    Create stonks.js

---
+++ commands/stonks.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "stonks",
+description: "Stonks Command",
+alias: [],
+run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+  
+    let user = message.mentions.users.first()
+    if (!user) user = message.author
+
+    let avatar = user.displayAvatarURL({ format: 'png' });
+
+  const helpembed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setImage("https://vacefron.nl/api/stonks?user=" + avatar)
+      .setFooter("Command used by " + message.author.tag)
+    message.channel.send(helpembed);
+}
+}
