cd5ff22893c1823974d90da9a8665fb42d3d9e19
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-09 10:02:54 +0530

    Create trash.js

---
+++ commands/trash.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "trash",
+description: "Trash Command",
+alias: [],
+run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+  
+    let user = message.mentions.users.first()
+    if (!user) user = message.author
+
+    let avatar = user.displayAvatarURL({ format: 'png' });
+
+  const helpembed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setImage("https://api.no-api-key.com/api/v2/trash?image=" + avatar)
+      .setFooter("Command used by " + message.author.tag)
+    message.channel.send(helpembed);
+}
+}
