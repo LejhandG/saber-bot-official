b2af9ff591050464f97e029bf42f1cc92fec3f32
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-11 19:28:12 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,143 +1,102 @@
-const { ReactionCollector } = require('discord.js-collector')
-const { Client, MessageEmbed } = require("discord.js");
-const client = new Client();
+const { MessageEmbed } = require("discord.js")
 
 module.exports = {
-    name: "help",
-description: "Show bot commands list",
-alias: [],
-run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play, client, Client) => {
-    const botMessage = await message.reply('**__Command List__**');
-    ReactionCollector.paginator({
-        botMessage,
-        user: message.author,
-        pages: [
-            new MessageEmbed()
-            .setColor("GOLD")
-            .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
-            .setTitle("Getting Started")
-            .setDescription(`
-\`setup\` , \`setupembed\`
+  name: "help",
+  description: "Show bot commands list",
+  alias: ["cmd", "command", "commands", "helps"],
+  run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+    const helpembed = new MessageEmbed()
+      .setColor("GOLD")
+      .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
+      .setDescription(
+        `
+__**Command list**__
 
+__Getting Started__
+\`setup\` , \`setupembed\`
+        
+__Music Commands__
+\`play\` > **\`play [title/url]\`**
+\`search\` > **\`search [title]\`**
+\`skip\`, \`stop\`,  \`pause\`, \`resume\`
+\`nowplaying\`, \`queue\`, \`volume\`
+\`lyrics\`, \`skipto\`, \`remove\`
+
+__Chat Commands__
+\`chat\`
+
+__Invites Commands__
+\`invites\`, \`serverinvites\`
+
+__Moderation Commands__
+\`ban\`, \`kick\`, \`mute\`, \`lock\`
+\`sofban\`, \`warn\`, \`unmute\`, \`role\`
+\`slowmode\`, \`createrole\`
+\`unmute\`, \`purge\`
+\`createtext\`, \`createvoice\`
+\`createcategory\`, \`hackban\`
+\`unban\`
+
+__Ticket Commands__
+\`tnew\`, \`tclose\`, \`trole\`
+\`tcategory\`, \`tadduser\`
+
+__Fun Commands__
+\`meme\`, \`roast\`
+\`8ball\`, \`dadjokes\`, \`coin\`
+
+__Image Commands__
+\`spongeburn\`, \`imgsay\`, \`cat\`
+\`slap\`, \`clap\`, \`hug\`
+\`duck\`, \`cry\`, \`kiss\`
+\`angry\`, \`mcachiv\`, \`cmy\`
+\`watermeme\`, \`mcdc\`, \`call\`
+\`emojigen\`, \`fact\`, \`captcha\`
+\`chalk\`, \`caution\`, \`genie\`
+\`ifthosekids\`, \`trumptweet\`
+\`shoot\`, \`stonks\`, \`disgusting\`
+\`grave\`, \`smart\`, \`scared\`
+\`buzz\`, \`wasted\`, \`trash\`
+\`blueify\`, \`simpcard\`, \`snow\`
+\`dog\`, \`cutecat\`
+
+__Search Commands__
+\`pokemon\`
+
+__Utility Commands__
+\`poll\`, \`shorten\`, \`weather\`
+\`hastebin\`
+
+__Info Commands__
+\`serverinfo\`, \`whois\`, \`avatar\`
+\`stats\`, \`roles\`
+
+__Misc Commands__
+\`invite\`, \`support\`, \`website\`
+\`setnick\`, \`botnick\`, \`donate\`
+\`syntax\`, \`vote\`
+
+__Embed Commands__
+\`say\`, \`advancedembed\`, \`rules\`
+\`announce\`, \`embed\`, \`imageembed\`
+
+__API Commands__
+\`ping\`, \`uptime\`
+
+__Suggest Commands__
+\`suggest\`, \`review\`
+
+__Bot Owner Only__
+\`servers\`, \`dm\`
+
+[**HOW TO USE OUR COMMANDS?**](https://pastebin.com/FQ2PzKjZ)
 [**INVITE OUR BOT**](https://discord.com/oauth2/authorize?client_id=751079643980890225&scope=bot&permissions=2146958847)
 [**CHECK OUT OUR WEBSITE**](https://saberofficial.ml/)
 [**DONATE US**](https://patreon.com/saberbot)
-[**JOIN OUR SUPPORT SERVER**](https://discord.gg/kBPpv47EJp)
-            `)
-            .setFooter("©️ 2020 Saber Bot"),
-            new MessageEmbed()
-            .setColor("GOLD")
-            .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
-            .setTitle("Music Commands")
-            .setDescription(`
-            \`play\` > **\`play [title/url]\`**
-            \`search\` > **\`search [title]\`**
-            \`skip\`, \`stop\`,  \`pause\`, \`resume\`
-            \`nowplaying\`, \`queue\`, \`volume\`
-            \`lyrics\`, \`skipto\`, \`remove\`
-            `)
-            .setFooter("©️ 2020 Saber Bot"),
-            new MessageEmbed()
-            .setColor("GOLD")
-            .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
-            .setTitle("Chat Commands")
-            .setDescription(`
-            \`chat\`
-            `)
-            .setFooter("©️ 2020 Saber Bot"),
-            new MessageEmbed()
-            .setColor("GOLD")
-            .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
-            .setTitle("Invite Commands")
-            .setDescription(`
-            \`invites\`, \`serverinvites\`
-            `)
-            .setFooter("©️ 2020 Saber Bot"),
-            new MessageEmbed()
-            .setColor("GOLD")
-            .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
-            .setTitle("Moderation Commands")
-            .setDescription(`
-            \`ban\`, \`kick\`, \`mute\`, \`lock\`
-            \`sofban\`, \`warn\`, \`unmute\`, \`role\`
-            \`slowmode\`, \`createrole\`
-            \`unmute\`, \`purge\`
-            \`createtext\`, \`createvoice\`
-            \`createcategory\`, \`hackban\`
-            \`unban\`
-            `)
-            .setFooter("©️ 2020 Saber Bot"),
-            new MessageEmbed()
-            .setColor("GOLD")
-            .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
-            .setTitle("Ticket Commands")
-            .setDescription(`
-            \`tnew\`, \`tclose\`, \`trole\`
-            \`tcategory\`, \`tadduser\`
-            `)
-            .setFooter("©️ 2020 Saber Bot"),
-            new MessageEmbed()
-            .setColor("GOLD")
-            .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
-            .setTitle("Fun Commands")
-            .setDescription(`
-            \`meme\`, \`roast\`, \`pokemon\`
-            \`8ball\`, \`dadjokes\`, \`coin\`            
-            `)
-            .setFooter("©️ 2020 Saber Bot"),
-            new MessageEmbed()
-            .setColor("GOLD")
-            .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
-            .setTitle("Image Commands")
-            .setDescription(`
-            \`spongeburn\`, \`imgsay\`, \`cat\`
-            \`slap\`, \`clap\`, \`hug\`
-            \`duck\`, \`cry\`, \`kiss\`
-            \`angry\`, \`mcachiv\`, \`cmy\`
-            \`watermeme\`, \`mcdc\`, \`call\`
-            \`emojigen\`, \`fact\`, \`captcha\`
-            \`chalk\`, \`caution\`, \`genie\`
-            \`ifthosekids\`, \`trumptweet\`
-            \`shoot\`, \`stonks\`, \`disgusting\`
-            \`grave\`, \`smart\`, \`scared\`
-            \`buzz\`, \`wasted\`, \`trash\`
-            \`blueify\`, \`simpcard\`, \`snow\`
-            \`dog\`, \`cutecat\`           
-            `)
-            .setFooter("©️ 2020 Saber Bot"),
-            new MessageEmbed()
-            .setColor("GOLD")
-            .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
-            .setTitle("Utility Commands")
-            .setDescription(`
-            \`poll\`, \`shorten\`, \`weather\`
-            \`hastebin\`          
-            `)
-            .setFooter("©️ 2020 Saber Bot"),
-            new MessageEmbed()
-            .setColor("GOLD")
-            .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
-            .setTitle("Info Commands")
-            .setDescription(`
-            \`serverinfo\`, \`whois\`, \`avatar\`
-            \`stats\`, \`roles\`          
-            `)
-            .setFooter("©️ 2020 Saber Bot"),
-            new MessageEmbed()
-            .setColor("GOLD")
-            .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
-            .setTitle("Misc Commands")
-            .setDescription(`
-            \`invite\`, \`support\`, \`website\`
-            \`setnick\`, \`botnick\`, \`donate\`
-            \`syntax\`, \`vote\`, \`ping\`, \`uptime\`
-            \`suggest\`, \`review\`         
-            `)
-            .setFooter("©️ 2020 Saber Bot")
-        ],
-        collectorOptions: {
-                time: 60000
-        }
-        });
-}
+[**JOIN OUR SUPPORT SERVER**](https://discord.gg/kBPpv47EJp)`
+      )
+      .setImage("https://i.imgur.com/CjNPEda.gif")
+      .setFooter("©️ 2020 Saber Bot");
+    message.channel.send(helpembed);
+  }
 }
