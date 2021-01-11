736df3b1c8ed9836d3df50a483f081469e772d75
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-11 17:00:35 +0530

    Create cutecat.js

---
+++ commands/cutecat.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js")
+const random = require('random')
+
+module.exports = {
+    name: "cat",
+description: "Cute Cat Commands",
+alias: [],
+run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+  
+const min = 1
+const max = 99999999
+
+  const helpembed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setImage("https://cataas.com/cat?" + random.int(min, max))
+      .setFooter("Command used by " + message.author.tag)
+    message.channel.send(helpembed);
+}
+}
