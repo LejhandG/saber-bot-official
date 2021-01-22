dfdae7b678517b5b8982b179d122825cb2112942
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-22 15:36:51 +0530

    Update botnick.js

--- commands/botnick.js
+++ commands/botnick.js
@@ -6,8 +6,22 @@
   description: "Changes Nickname of the Bot",
   alias: [],
   run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+    
+    let wrong = new MessageEmbed()
+        .setTitle(`Command: /botnick`)
+        .setDescription(`
+**Description:** 
+It changes the nickname of the bot.
+**Usage:**
+/botnick [name]
+**Example:**
+/botnick Saber
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
+    
     let name = args.slice(1).join(' ');
-    if (name.length < 1) return message.reply('Write a name for the bot !!');
+    if (name.length < 1) return message.reply(wrong);
 
     if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('You cannot change my nickname');
       message.guild.me.setNickname(message.content.replace('/botnick ', ''));
