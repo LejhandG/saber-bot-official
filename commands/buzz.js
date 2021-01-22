e8800a1e45dad07f0748969d8c5eb8852b8248e2
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-22 15:45:46 +0530

    Update buzz.js

--- commands/buzz.js
+++ commands/buzz.js
@@ -5,14 +5,27 @@ module.exports = {
 description: "Buzz Command",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
-  
+    
+    let wrong = new MessageEmbed()
+        .setTitle(`Command: /buzz`)
+        .setDescription(`
+**Description:** 
+Adds text to toy story meme
+**Usage:**
+/buzz [text]
+**Example:**
+/buzz Hello
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
+    
   let text = args.slice(1).join(' ');
-  if (text.length < 1) return message.reply('Write some text !!');
+  if (text.length < 1) return message.reply(wrong);
 
   const helpembed = new MessageEmbed()
       .setColor("RANDOM")
       .setImage("https://api.memegen.link/images/buzz/" + args.slice(1, args.length + 1).join(" ") + ".png")
-      .setFooter("Command used by " + message.author.tag)
+      .setFooter(message.author.tag, message.author.avatarURL())
     message.channel.send(helpembed);
 }
 }
