d536020cea389a318246a4bf8116babe7f9c01e6
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-22 17:05:49 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,10 +4,33 @@ module.exports = {
   alias: [],
   run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
     
+    let wrong = new MessageEmbed()
+        .setTitle(`Command: /ban`)
+        .setDescription(`
+**Description:** 
+It bans the user from the server
+**Usage:**
+/ban [user] [reason]
+**Example:**
+/ban @Vortex They were bad
+`)
+    .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
+    
     if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("You don't have premission to do that!");
     
     const user = message.mentions.users.first();
     let reason1 = args.slice(2).join(' ');
+    
+    let banlog = new MessageEmbed()
+        .setTitle(`Member Banned`)
+        .setDescription(`
+**Member Banned** - ${user}
+**Moderator** - ${message.author.tag}
+**Reason** - ${reason1}
+`)
+    .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
 
     if (user === message.author) return message.channel.send("You can't ban yourself.");
 
@@ -29,7 +52,7 @@ module.exports = {
         message.reply("That user isn't in this guild!");
       }
     } else {
-      message.reply("You didn't mention the user to ban!");
+      message.reply(wrong);
     }
   }
 }
