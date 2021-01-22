6022f23b941713cdbcdffeac8563469030164058
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-22 15:04:27 +0530

    Update blueify.js

--- commands/blueify.js
+++ commands/blueify.js
@@ -13,6 +13,7 @@ run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQ
 
   const helpembed = new MessageEmbed()
       .setColor("RANDOM")
+      .setTitle("Blueify")
       .setImage("https://api.no-api-key.com/api/v2/blueify?image=" + avatar)
       .setFooter(message.author.tag, message.author.avatarURL())
     message.channel.send(helpembed);
