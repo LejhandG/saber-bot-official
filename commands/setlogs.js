113a0d6369cb95381802cbf05b44ef80dca798eb
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-22 15:21:38 +0530

    Create setlogs.js

---
+++ commands/setlogs.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js')
+const db = require("quick.db")
+
+module.exports = {
+    name: "setlogs",
+description: "Set the logs channel",
+alias: [],
+run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+  
+  let permission = message.member.hasPermission("ADMINISTRATOR");
+
+if(!permission) return message.channel.send("You are missing the permission `ADMINISTRATOR`")
+
+ let cArgs = args[1]
+ 
+ if(isNaN(cArgs)) return message.channel.send("You must specify a valid id for the welcome channel!")
+	 
+ try{
+	 bot.guilds.get(message.guild.id).channels.get(cArgs).send("Welcome channel set!")
+	 
+ db.set(`${message.guild.id}`, cArgs)
+ 
+ message.channel.send("You have successfully set the welcome channel to <#" + cArgs + ">")
+return;
+ }catch(e){
+	return message.channel.send("Error: missing permissions or channel doesn't exist")
+ }
+}
+}
