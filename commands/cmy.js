f649b42bfaac714c792aad7265109bd4a906f303
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-25 19:50:46 +0530

    Update cmy.js

--- commands/cmy.js
+++ commands/cmy.js
@@ -5,14 +5,28 @@ module.exports = {
 description: "Change My Mind",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+    
+    let wrong = new MessageEmbed()
+        .setTitle(`Command: /cmy`)
+        .setDescription(`
+**Description:** 
+Change my mind meme format
+**Usage:**
+/cmy [text]
+**Example:**
+/cmy Lol
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
+
   
   let text = args.slice(1).join(' ');
-  if (text.length < 1) return message.reply('Write some text !!');
+  if (text.length < 1) return message.reply(wrong);
 
   const helpembed = new MessageEmbed()
       .setColor("RANDOM")
       .setImage("https://vacefron.nl/api/changemymind?text=" + args.slice(1, args.length + 1).join("+"))
-      .setFooter("Command used by " + message.author.tag)
+      .setFooter(message.author.tag, message.author.avatarURL())
     message.channel.send(helpembed);
 }
 }
