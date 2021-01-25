fe9a903df61b49f705bc64616f74ee4ae5b1ca4a
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-25 19:58:58 +0530

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -6,9 +6,6 @@ description: "Cat with Text",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
   
-  let text = args.slice(1).join(' ');
-  if (text.length < 1) return message.reply('Write some text !!');
-  
   const helpembed = new MessageEmbed()
       .setColor("RANDOM")
       .setImage("https://cataas.com/cat/says/" + args.slice(1, args.length + 1).join("+"))
