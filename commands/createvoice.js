420cff16e4d865070fd32de6136814a9767d47cf
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-29 16:50:42 +0530

    Update createvoice.js

--- commands/createvoice.js
+++ commands/createvoice.js
@@ -1,19 +1,38 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
     name: "createvoice",
 description: "Creates voice channel",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
-  
-  let text = args.slice(1).join(' ');
-  if (text.length < 1) return message.reply('Write name of the voice channel !!');
-  
-if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send('You do not have the required permission to use this command.').then(m => {
+    
+  let wrong = new MessageEmbed()
+        .setTitle(`Command: /createvoice`)
+        .setDescription(`
+**Description:** 
+Creates a text channel with the name
+**Usage:**
+/createvoice [category id] [name]
+**Example:**
+/createvoice 789450070457319425 test
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
+    
+    if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send('You do not have the required permission to use this command.').then(m => {
         setTimeout(() => {
             m.delete()
         }, 3000);
     })
+  
+  let text = args.slice(2).join(' ');
+  let categoryid = args[1];
+  if (text.length < 1) return message.reply(wrong);
+  if (categoryid.length < 1) return message.reply(wrong);
     
-  message.guild.channels.create(args.slice(1, args.length + 1).join(" "), { type : 'voice'});
-  message.channel.send('Channel Created !!!');  
+  message.guild.channels.create(text, { type : 'voice'}).then(channel => {
+        channel.setParent(categoryid);
+    })
+  message.channel.send('Channel ' + text + ' created by ' + message.author.tag);  
 }
 }
