631a4cf044b8a39dcd18b985f0b2242b22c5267a
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-29 14:08:30 +0530

    Update createcategory.js

--- commands/createcategory.js
+++ commands/createcategory.js
@@ -3,17 +3,17 @@ module.exports = {
 description: "Creates category channel",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
-  
-  let text = args.slice(1).join(' ');
-  if (text.length < 1) return message.reply('Write the category name !!');
-  
-if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send('You do not have the required permission to use this command.').then(m => {
+    
+  if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send('You do not have the required permission to use this command.').then(m => {
         setTimeout(() => {
             m.delete()
         }, 3000);
     })
+  
+  let text = args.slice(1).join(' ');
+  if (text.length < 1) return message.reply('Write the category name');
     
   message.guild.channels.create(args.slice(1, args.length + 1).join(" "), { type : 'category'});
-  message.channel.send('Category Created !!!');  
+  message.channel.send('Category' + text 'Created by' + message.author.tag);  
 }
 }
