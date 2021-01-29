8574acd4bf6802f0f9fb63b7e733e93b9e3c5603
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-29 14:29:17 +0530

    Update createrole.js

--- commands/createrole.js
+++ commands/createrole.js
@@ -13,31 +13,25 @@ run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQ
 **Description:** 
 Create's a role in the guild
 **Usage:**
-/createrole [hex] [name]
+/createrole [name]
 **Example:**
-/createrole #00f9ff Moderator
+/createrole Moderator
 `)
         .setFooter(message.author.tag, message.author.avatarURL())
         .setColor(`RANDOM`);
-
-  
-  let text = args.slice(2).join(' ');
-  if (text.length < 1) return message.reply('Write name of the role');
     
-  let colour = args.slice(1).join(' ');
-  if (colour.length < 1) return message.reply();
-  
-  if (!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send('You do not have the required permission to use this command.').then(m => {
+    if (!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send('You do not have the required permission to use this command.').then(m => {
         setTimeout(() => {
             m.delete()
         }, 3000);
     })
+
+  
+  let text = args.slice(1).join(' ');
+  if (text.length < 1) return message.reply('Write name of the role');
   
 let guild = message.member.guild; let rolename = args.slice(1).join(" ");
-message.guild.roles.create({ data: 
-                            name: rolename
-                            color: colour
-                           });
+message.guild.roles.create({ data: { name: rolename } });
   message.channel.send("Role " + rolename + " created by " + message.author.tag);
 }
 }
