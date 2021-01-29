eb7277fa438a61286f129a030f55af3a04729c37
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-29 14:13:32 +0530

    Update createcategory.js

--- commands/createcategory.js
+++ commands/createcategory.js
@@ -9,9 +9,22 @@ run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQ
             m.delete()
         }, 3000);
     })
+    
+  let wrong = new MessageEmbed()
+        .setTitle(`Command: /createcategory`)
+        .setDescription(`
+**Description:** 
+Creates a category of the name
+**Usage:**
+/createcategory [name]
+**Example:**
+/createcategory DASHBOARD
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
   
   let text = args.slice(1).join(' ');
-  if (text.length < 1) return message.reply('Write the category name');
+  if (text.length < 1) return message.reply(wrong);
     
   message.guild.channels.create(args.slice(1, args.length + 1).join(" "), { type : 'category'});
   message.channel.send('Category ' + text + ' created by ' + message.author.tag);  
