3814c50b8ca03ef5c31126fab8d3ebcfa12e5cc1
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-01-29 14:22:17 +0530

    Update createrole.js

--- commands/createrole.js
+++ commands/createrole.js
@@ -1,13 +1,31 @@
 const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: "createrole",
 description: "Creates a new role",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+    
+  let wrong = new MessageEmbed()
+        .setTitle(`Command: /createrole`)
+        .setDescription(`
+**Description:** 
+Create's a role in the guild
+**Usage:**
+/createrole [hex] [name]
+**Example:**
+/createrole #00f9ff Moderator
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
+
   
-  let text = args.slice(1).join(' ');
-  if (text.length < 1) return message.reply('Write name of the role !!');
+  let text = args.slice(2).join(' ');
+  if (text.length < 1) return message.reply('Write name of the role');
+    
+  let colour = args.slice(1).join(' ');
+  if (colour.length < 1) return message.reply();
   
   if (!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send('You do not have the required permission to use this command.').then(m => {
         setTimeout(() => {
@@ -16,7 +34,10 @@ run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQ
     })
   
 let guild = message.member.guild; let rolename = args.slice(1).join(" ");
-message.guild.roles.create({ data: { name: rolename } });
-  message.channel.send("Role Created !!!");
+message.guild.roles.create({ data: 
+                            name: rolename
+                            color: colour
+                           });
+  message.channel.send("Role " + rolename + " created by " + message.author.tag);
 }
 }
