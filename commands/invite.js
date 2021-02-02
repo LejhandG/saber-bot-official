811cb11deebb7d76205c9a8d3977eff90dd86fd0
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-02 10:57:03 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -7,6 +7,7 @@ module.exports = {
   run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
      const helpembed = new MessageEmbed()
       .setColor("GOLD")
+      .setImage("https://cdn.discordapp.com/attachments/565818858938826772/806030793210200124/20210202_104336.png")
       .setDescription(
         "[**INVITE OUR BOT**](https://discord.com/oauth2/authorize?client_id=751079643980890225&scope=bot&permissions=2146958847)"
       );
