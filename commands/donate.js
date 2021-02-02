9432d96861bea4b804903ff3197b761fb87e8bc9
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-02 10:57:45 +0530

    Update donate.js

--- commands/donate.js
+++ commands/donate.js
@@ -7,6 +7,7 @@ module.exports = {
   run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
     const helpembed = new MessageEmbed()
       .setColor("GOLD")
+      .setImage("https://cdn.discordapp.com/attachments/565818858938826772/806030793558065162/20210202_104407.png")
       .setDescription(
         "[**DONATE US**](https://patreon.com/saberbot)"
       );
