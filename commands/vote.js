bc22bb350d81acca3b5d6ae65ca082281491d195
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-02 10:59:05 +0530

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -7,6 +7,7 @@ module.exports = {
   run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
     const helpembed = new MessageEmbed()
       .setColor("GOLD")
+      .setImage("https://cdn.discordapp.com/attachments/565818858938826772/806030794148937728/20210202_104741.png")
       .setDescription(
         "[**VOTE OUR BOT**](https://top.gg/bot/751079643980890225/vote)"
       );
