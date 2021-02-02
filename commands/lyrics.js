c1be0970dfd283684a07e4af2a6f9a8125f366d1
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-02 20:37:47 +0530

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -6,9 +6,11 @@ description: "Shows the lyrics of the song",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
   let name = args.slice(1).join(' ');
-  if (name.length < 1) return message.reply('Write the name of the song !!');
+  if (name.length < 1) return message.reply('Write the name of the song');
   
   var lyrics = await solenolyrics.requestLyricsFor(args.slice(1, args.length + 1).join(" "));
-  message.channel.send(lyrics, { split: true });
+  message.channel.send(lyrics, { split: true }).catch(err => {
+            message.reply("Not able to find the song");
+          });
 }
 }
