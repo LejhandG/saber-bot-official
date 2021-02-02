04df8e3ff422cdbcdf3901d14ecbf6ec2380db01
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-02 15:13:44 +0530

    Update imgsay.js

--- commands/imgsay.js
+++ commands/imgsay.js
@@ -5,14 +5,27 @@ module.exports = {
 description: "Image Text",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+    
+    let wrong = new MessageEmbed()
+        .setTitle(`Command: /imgsay`)
+        .setDescription(`
+**Description:** 
+Turns text to image
+**Usage:**
+/imgsay [text]
+**Example:**
+/imgsay Hello
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
   
     let name = args.slice(1).join(' ');
-  if (name.length < 1) return message.reply('Write some text to get a stylish text !!');
+  if (name.length < 1) return message.reply(wrong);
   
   const helpembed = new MessageEmbed()
       .setColor("RANDOM")
-      .setImage("https://flamingtext.com/net-fu/proxy_form.cgi?script=3d-logo&text=" + args.slice(1, args.length + 1).join("+") + "+&_loc=generate&imageoutput=true")
-      .setFooter("Command used by " + message.author.tag)
+      .setImage("https://flamingtext.com/net-fu/proxy_form.cgi?script=crafts-logo&text=" + args.slice(1, args.length + 1).join("+") + "+&_loc=generate&imageoutput=true")
+      .setFooter(message.author.tag, message.author.avatarURL())
     message.channel.send(helpembed);
 }
 }
