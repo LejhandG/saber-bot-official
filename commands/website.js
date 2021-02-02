89e969c9f1aa72fd574c68157ed96abdd81178ec
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-02 10:58:27 +0530

    Update website.js

--- commands/website.js
+++ commands/website.js
@@ -7,6 +7,7 @@ module.exports = {
   run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
     const helpembed = new MessageEmbed()
       .setColor("GOLD")
+      .setImage("https://cdn.discordapp.com/attachments/565818858938826772/806030793893216276/20210202_104436.png")
       .setDescription(
         "[**CHECK OUT OUR WEBSITE**](https://saberofficial.ml/)"
       );
