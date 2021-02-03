485354c436080a98007626aff282870d02f7de20
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-03 10:43:23 +0530

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,12 +1,27 @@
-const solenolyrics= require("solenolyrics"); 
+const solenolyrics= require("solenolyrics");
+const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     name: "lyrics",
 description: "Shows the lyrics of the song",
-alias: [],
+alias: ["lyric"],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+    
+    let wrong = new MessageEmbed()
+        .setTitle(`Command: /lyrics`)
+        .setDescription(`
+**Description:** 
+Sends the lyrics of the mentioned song
+**Usage:**
+/lyrics [song name]
+**Example:**
+/lyrics Rap God
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
+    
   let name = args.slice(1).join(' ');
-  if (name.length < 1) return message.reply('Write the name of the song');
+  if (name.length < 1) return message.reply(wrong);
   
   var lyrics = await solenolyrics.requestLyricsFor(args.slice(1, args.length + 1).join(" "));
   message.channel.send(lyrics, { split: true }).catch(err => {
