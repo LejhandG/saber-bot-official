f5a2fe6b1405c3f87568b9f7c7f0406658789437
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-03 19:39:57 +0530

    Update trole.js

--- commands/trole.js
+++ commands/trole.js
@@ -9,11 +9,24 @@ module.exports = {
 description: "command description",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+ 
+ let wrong = new MessageEmbed()
+        .setTitle(`Command: /trole`)
+        .setDescription(`
+**Description:** 
+Set the role which can see the tickets
+**Usage:**
+/trole @role
+**Example:**
+/trole @Staff
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
 
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You don't have premission to do that!");
 
     let text = args.slice(1).join(' ');
-   if (text.length < 1) return message.reply('Mention the role that can access the channel !!');
+   if (text.length < 1) return message.reply(wrong);
 
     const role = message.mentions.roles.first()
  
