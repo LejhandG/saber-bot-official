da016212da08586d5d9e6cd07b50a7dc7b28b3b8
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-03 19:09:42 +0530

    Update tadduser.js

--- commands/tadduser.js
+++ commands/tadduser.js
@@ -9,14 +9,27 @@ module.exports = {
 description: "Adds user to ticket",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+ 
+ let wrong = new MessageEmbed()
+        .setTitle(`Command: /tadduser`)
+        .setDescription(`
+**Description:** 
+Adds the user to a existing ticket
+**Usage:**
+/tadduser #channel @user
+**Example:**
+/tadduser #ticket-1 @Vortex
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
 
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You don't have premission to do that!");
 
     let text = args.slice(1).join(' ');
-   if (text.length < 1) return message.reply('Mention a user to add to the channel !!');
+   if (text.length < 1) return message.reply(wrong);
 
    let ok = args.slice(2).join(' ');
-   if (ok.length < 1) return message.reply('Mention the ticket !!');
+   if (ok.length < 1) return message.reply(wrong);
 
     const channel = message.mentions.channels.first()
         const user = message.mentions.users.first()
