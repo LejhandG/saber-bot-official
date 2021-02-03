82f9c89d29c2d7f52d0642299510842b373c679f
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-03 17:59:14 +0530

    Update smart.js

--- commands/smart.js
+++ commands/smart.js
@@ -13,8 +13,9 @@ run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQ
 
   const helpembed = new MessageEmbed()
       .setColor("RANDOM")
+      .setTitle("Smart Command")
       .setImage("https://api.no-api-key.com/api/v2/smrt?image=" + avatar)
-      .setFooter("Command used by " + message.author.tag)
+      .setFooter(message.author.tag, message.author.avatarURL())
     message.channel.send(helpembed);
 }
 }
