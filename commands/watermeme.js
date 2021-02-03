d763504abc4034ec11a2d5b99a0a0994427297bc
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-03 20:12:34 +0530

    Update watermeme.js

--- commands/watermeme.js
+++ commands/watermeme.js
@@ -5,9 +5,22 @@ module.exports = {
 description: "WaterMeme",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+    
+    let wrong = new MessageEmbed()
+        .setTitle(`Command: /watermeme`)
+        .setDescription(`
+**Description:** 
+Adds text to the watermeme format
+**Usage:**
+/watermeme [text]
+**Example:**
+/watermeme Lol
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
   
     let name = args.slice(1).join(' ');
-  if (name.length < 1) return message.reply('Write some text !!');
+  if (name.length < 1) return message.reply(wrong);
 
   const helpembed = new MessageEmbed()
       .setColor("RANDOM")
