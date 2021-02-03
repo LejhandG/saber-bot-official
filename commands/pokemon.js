31c81027e80753d1024501e7c0e77b300cf73519
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-03 12:28:50 +0530

    Update pokemon.js

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -5,6 +5,19 @@ module.exports = {
 description: "Finds Pokemon",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+    
+    let wrong = new MessageEmbed()
+        .setTitle(`Command: /pokemon`)
+        .setDescription(`
+**Description:** 
+Finds the pokemon mentioned by you
+**Usage:**
+/pokemon [name]
+**Example:**
+/pokemon Pikachu
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
   
     let name = args.slice(1).join(' ');
   if (name.length < 1) return message.reply('Write a name of the pokemon !!');
@@ -14,6 +27,8 @@ run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQ
       .setTitle("A Pokemon")
       .setImage("https://i.some-random-api.ml/pokemon/" + args.slice(1, args.length + 1).join("+") + ".png")
       .setFooter("Command used by " + message.author.tag)
-    message.channel.send(helpembed);
+    message.channel.send(helpembed).catch(err => {
+            message.reply("An error occured");
+          });
 }
 }
