5076e8c6a3b953224b1ff22b86409add4f618a2e
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-03 19:17:39 +0530

    Update tcategory.js

--- commands/tcategory.js
+++ commands/tcategory.js
@@ -9,11 +9,24 @@ module.exports = {
 description: "Sets ticket category",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+ 
+ let wrong = new MessageEmbed()
+        .setTitle(`Command: /tcategory`)
+        .setDescription(`
+**Description:** 
+Sets the category of the ticket
+**Usage:**
+/tcategory [category id]
+**Example:**
+/tcategory 69814629264426
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
 
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You don't have premission to do that!");
 
     let text = args.slice(1).join(' ');
-   if (text.length < 1) return message.reply('Write the ID of the category !!');
+   if (text.length < 1) return message.reply(wrong);
 
     const ID = args.slice(1).join(" ")
     ticket.Category(message, ID)
