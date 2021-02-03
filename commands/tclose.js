a9a51f090dbec5298cdb677a7b4d694b1b29cb29
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-03 19:22:19 +0530

    Update tclose.js

--- commands/tclose.js
+++ commands/tclose.js
@@ -9,10 +9,24 @@ module.exports = {
 description: "Closes the ticket",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+ 
+ let wrong = new MessageEmbed()
+        .setTitle(`Command: /tclose`)
+        .setDescription(`
+**Description:** 
+Closes the existing ticket
+**Usage:**
+/tclose #channel
+**Example:**
+/tclose #ticket-1
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
+ 
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You don't have premission to do that!");
 
     let text = args.slice(1).join(' ');
-   if (text.length < 1) return message.reply('Mention the ticket to close !!');
+   if (text.length < 1) return message.reply(wrong);
 
     const channel = message.mentions.channels.first() || message.guild.channels.cache.find(c => c.id == args || c.name == args) || message.channel
     ticket.closeTicket(message, channel)
