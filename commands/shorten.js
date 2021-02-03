19fe962b0d09e9338b04d1a0773795df5aa8e2a7
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-03 17:24:27 +0530

    Update shorten.js

--- commands/shorten.js
+++ commands/shorten.js
@@ -5,14 +5,29 @@ module.exports = {
 description: "Shortens a Link",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+    
+    let wrong = new MessageEmbed()
+        .setTitle(`Command: /shorten`)
+        .setDescription(`
+**Description:** 
+Shortens the given link
+**Usage:**
+/shorten [link]
+**Example:**
+/shorten https://youtube.com/
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
   
-  if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You don't have premission to do that!");
+  if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You don't have premission to do that");
   
     let name = args.slice(1).join(' ');
-  if (name.length < 1) return message.reply('Please paste the link with the command !!');
+  if (name.length < 1) return message.reply(wrong);
   
   adfly.short(args.slice(1, args.length + 1).join(" "),function(url){
-    message.channel.send("Short URL is: "+url);
+    message.channel.send("Short URL is: "+url).catch(err => {
+            message.reply("Not a valid link");
+          });
 });
 }
 }
