070093b15209205fa9103bbbe52f34df5457e69d
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-03 19:31:43 +0530

    Update tnew.js

--- commands/tnew.js
+++ commands/tnew.js
@@ -9,11 +9,24 @@ module.exports = {
 description: "Creates a new ticket",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+ 
+ let wrong = new MessageEmbed()
+        .setTitle(`Command: /tnew`)
+        .setDescription(`
+**Description:** 
+Creates a new ticket
+**Usage:**
+/tnew [reason]
+**Example:**
+/tnew Want to contact staff
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
 
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You don't have premission to do that!");
 
     let text = args.slice(1).join(' ');
-   if (text.length < 1) return message.reply('Write the reason of the ticket !!');
+   if (text.length < 1) return message.reply(wrong);
 
     const reason = args.slice(1).join(" ")
     ticket.makeTicket(message, reason, "swrf")
