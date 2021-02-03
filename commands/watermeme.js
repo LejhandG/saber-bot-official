d2e00f0d19867ec187e427c007e318a8583635d9
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-03 19:52:42 +0530

    Update watermeme.js

--- commands/watermeme.js
+++ commands/watermeme.js
@@ -11,8 +11,9 @@ run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQ
 
   const helpembed = new MessageEmbed()
       .setColor("RANDOM")
+      .setTitle("Watermeme Command")
       .setImage("https://vacefron.nl/api/water?text=" + args.slice(1, args.length + 1).join("+"))
-      .setFooter("Command used by " + message.author.tag)
+      .setFooter(message.author.tag, message.author.avatarURL())
     message.channel.send(helpembed);
 }
 }
