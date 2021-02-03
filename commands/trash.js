f7efd50591dcff5830e795bd30fc07f08f0fc14f
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-03 19:33:48 +0530

    Update trash.js

--- commands/trash.js
+++ commands/trash.js
@@ -13,8 +13,9 @@ run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQ
 
   const helpembed = new MessageEmbed()
       .setColor("RANDOM")
+      .setTitle("Trash Command")
       .setImage("https://api.no-api-key.com/api/v2/trash?image=" + avatar)
-      .setFooter("Command used by " + message.author.tag)
+      .setFooter(message.author.tag, message.author.avatarURL())
     message.channel.send(helpembed);
 }
 }
