7de67a8844ba1d76a3598cd18b02dfc25ca3e0b2
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-03 10:45:23 +0530

    Update mcachiv.js

--- commands/mcachiv.js
+++ commands/mcachiv.js
@@ -5,15 +5,29 @@ module.exports = {
 description: "MC Achievement",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+    
+  let wrong = new MessageEmbed()
+        .setTitle(`Command: /mcachiv`)
+        .setDescription(`
+**Description:** 
+Sends and image of a minecraft achivement with your text
+**Usage:**
+/mcachiv [text]
+**Example:**
+/mcachiv Nether Fortress
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
+
   
   let name = args.slice(1).join(' ');
-  if (name.length < 1) return message.reply('Write some text to make it an minecraft achivement !!');
+  if (name.length < 1) return message.reply(wrong);
   
   const helpembed = new MessageEmbed()
       .setColor("RANDOM")
       .setTitle("New Achievement!")
       .setImage("https://minecraftskinstealer.com/achievement/2/New+achievement%21/" + args.slice(1, args.length + 1).join("+"))
-      .setFooter("Command used by " + message.author.tag)
+      .setFooter(message.author.tag, message.author.avatarURL())
     message.channel.send(helpembed);
 }
 }
