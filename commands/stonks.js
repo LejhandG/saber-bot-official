0b27f5a678e97add3a038deaef2f203a2474ee11
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-03 18:58:43 +0530

    Update stonks.js

--- commands/stonks.js
+++ commands/stonks.js
@@ -13,8 +13,9 @@ run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQ
 
   const helpembed = new MessageEmbed()
       .setColor("RANDOM")
+      .setTitle("Stonks Command")
       .setImage("https://vacefron.nl/api/stonks?user=" + avatar)
-      .setFooter("Command used by " + message.author.tag)
+      .setFooter(message.author.tag, message.author.avatarURL())
     message.channel.send(helpembed);
 }
 }
