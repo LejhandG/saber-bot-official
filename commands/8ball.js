2bcad740f0b31daef7627f670877adc9a54b353e
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-04 18:32:52 +0530

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -45,7 +45,30 @@ Its gives an answer to your question
         "A huge chance",
         "A small chance",
         "You better hope so",
-        "You better not hope so"
+        "You better not hope so",
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes - definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "doubt",
+        "lmao no",
+        "yes 😅",
+        "Very doubtful."
       ];
       let response =
         responses[Math.floor(Math.random() * responses.length - 1)];
