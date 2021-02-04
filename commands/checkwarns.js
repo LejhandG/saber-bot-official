ebfedca1b68094ed40d809a8347e81539238cb10
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-04 14:18:58 +0530

    Create checkwarns.js

---
+++ commands/checkwarns.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const fs = require("fs");
+const db = require('quick.db')
+
+module.exports = {
+    name: "checkwarns",
+description: "Check warns of the user",
+alias: [],
+run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+
+    const user = message.mentions.members.first() || message.author
+    let warnings = db.get(`warnings_${message.guild.id}_${user.id}`)
+
+    if(warnings === null) warnings = 0;
+
+    message.channel.send(`He/She has **${warnings}** warning(s)`)
+}
+}
