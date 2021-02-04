9740c4413747cdea7ac37b3d43fb3d315f8bcfd8
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-04 19:15:43 +0530

    Create djs.js

---
+++ commands/djs.js
@@ -0,0 +1,47 @@
+const { MessageEmbed } = require("discord.js")
+const Discord = require('discord.js')
+const fetch = require('node-fetch');
+
+module.exports = {
+    name: "djs",
+description: "Discord JS Docs",
+alias: ["discordjs"],
+run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+
+let wrong = new MessageEmbed()
+        .setTitle(`Command: /djs`)
+        .setDescription(`
+**Description:** 
+Shows from discordjs library/docs
+**Usage:**
+/djs [text]
+**Example:**
+/djs MessageEmbed
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
+
+let query = args.slice(1).join(" ");
+
+                // Input Checking
+                if (!query[0]) { message.channel.send(wrong) } else {
+
+
+                    // Executing
+                    const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${query}`;
+                    fetch(url)
+                        .then(res => res.json())
+                        .then(embed => {
+                            if (embed && !embed.error) {
+                                message.channel.send({ embed });
+                            } else {
+                                message.reply(`Failed to find anything using the specified query in DiscordJS library. Please try again.`);
+                            }
+                        })
+                        .catch(err => {
+                            this.client.logger.error(err);
+                            message.reply('An error occured');
+                        });
+                }
+}
+}
