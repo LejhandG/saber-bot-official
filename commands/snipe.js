937ecb75000342b592819127cb833aad05a32f00
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-05 11:09:34 +0530

    Create snipe.js

---
+++ commands/snipe.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require('discord.js')
+const Discord = require('discord.js')
+
+module.exports = {
+    name: "snipe",
+description: "Shows the deleted message in the channel",
+alias: [],
+run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+    const msg = bot.snipes.get(message.channel.id)
+    if(!msg) return message.channel.send("There are no deleted messages in this channel!")
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(msg.author)
+    .setDescription(msg.content)
+    .setColor("RANDOM")
+    if(msg.image)embed.setImage(msg.image)
+    
+    message.channel.send(embed)
+}
+}
