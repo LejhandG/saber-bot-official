273b9b35b3402da01b21fa34164254a9d315ba74
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-05 11:15:55 +0530

    Create npm.js

---
+++ commands/npm.js
@@ -0,0 +1,42 @@
+const { MessageEmbed } = require("discord.js")
+const Discord = require('discord.js')
+const fetch = require('node-fetch');
+
+module.exports = {
+    name: "npm",
+description: "NPM Packages",
+alias: [],
+run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+
+let wrong = new MessageEmbed()
+        .setTitle(`Command: /npm`)
+        .setDescription(`
+**Description:** 
+Shows from npm packages
+**Usage:**
+/npm [text]
+**Example:**
+/npm discord.js
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
+        
+        try {
+      const query = args.slice(1).join(" ")
+      const data = await fetch("https://registry.npmjs.org/" + query.toLowerCase()).then(lang => lang.json()) 
+      let e = new Discord.MessageEmbed()
+      e.setAuthor("NPM")
+      e.setTitle(data.name)
+      e.addField("Description",data.description)
+      e.addField("Author",data.author.name)
+      e.addField("License",data.license)
+      e.addField("Keywords",data.keywords.join(", "))
+      e.setColor("RANDOM")
+      e.setFooter(`Requested by ${msg.author.username}`,msg.author.displayAvatarURL())
+      e.setTimestamp()
+      msg.say(e)
+    } catch (error) {
+      msg.say("Npm package not found")
+    }
+}
+}
