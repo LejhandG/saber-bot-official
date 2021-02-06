3acdb46dead70f993a7b8a7374fe1b590765e193
LejhandGamingYT <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-06 15:42:23 +0530

    Update setprefix.js

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -25,7 +25,7 @@ It sets the prefix for your guild
     if (!args[1]) return message.channel.send(wrong)
     if (args[1].length > 3) return message.channel.send("A prefix can only be 3 or less characters")
     if (args[1] === db.get(`guild_${message.guild.id}_prefix`)) return message.channel.send("That is already your prefix")
-    if (args[1] === ";") db.delete(`guild_${message.guild.id}_prefix`)
+    if (args[1] === "/") db.delete(`guild_${message.guild.id}_prefix`)
     db.set(`guild_${message.guild.id}_prefix`, args[1])
     return message.channel.send(`I have now set your prefix to ${args[1]}`)
 }
