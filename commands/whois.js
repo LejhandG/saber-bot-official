10d191e40f5bad1d7795a9ad63e390c351eb2776
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-07 23:51:08 +0530

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -3,6 +3,7 @@ const { MessageEmbed } = require('discord.js')
 
 module.exports = {
   name: "whois",
+  timeout : 2000,
 description: "Find the info about the mentioned user",
 alias: ["userinfo"],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
