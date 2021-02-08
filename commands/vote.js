495ece8ab21723b51c34d18826de3253a2959495
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 10:32:09 +0530

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = {
   name: "vote",
+  timeout : 3000,
   description: "Upvote bot in topgg",
   alias: [],
   run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
