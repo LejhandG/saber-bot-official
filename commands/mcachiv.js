edc20c28e05962934b7b02fa5d456416cdc4e0ea
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 10:02:59 +0530

    Update mcachiv.js

--- commands/mcachiv.js
+++ commands/mcachiv.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require('discord.js')
 
 module.exports = {
     name: "mcachiv",
+    timeout : 3000,
 description: "MC Achievement",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
