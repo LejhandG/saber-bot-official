ef649b72672268a87221e69b634693b179515b16
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 09:48:00 +0530

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require('discord.js')
 
 module.exports = {
   name: "avatar",
+  timeout : 3000,
   description: "Show your avatar",
   alias: ["ava", "avatar", "av"],
   run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
