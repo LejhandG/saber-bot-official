866339253a5cd6fbefa2b742e77efc7f1c382a34
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 10:03:37 +0530

    Update mcdc.js

--- commands/mcdc.js
+++ commands/mcdc.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     name: "mcdc",
+    timeout : 3000,
 description: "Minecraft Disconnect",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
