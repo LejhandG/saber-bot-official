408078e83bb1e5be6a8074e1268de1d205e8817b
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 10:01:25 +0530

    Update invites.js

--- commands/invites.js
+++ commands/invites.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require('discord.js')
 
 module.exports = {
     name: "invites",
+    timeout : 3000,
 description: "Check the invites",
 alias: ["checkinvites"],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
