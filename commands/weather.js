7ae6b7c44ad7e25161999bcf9ef3729f62fba95f
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 10:33:25 +0530

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     name: "weather",
+    timeout : 3000,
 description: "Shows the weather of the region",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
