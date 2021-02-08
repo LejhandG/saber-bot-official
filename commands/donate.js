572db7b5e647a7e2d9c9af22ae6d8fc3d3e0ef0d
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 09:57:41 +0530

    Update donate.js

--- commands/donate.js
+++ commands/donate.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = {
   name: "donate",
+  timeout : 3000,
   description: "Patreon Link",
   alias: ["patreon"],
   run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
