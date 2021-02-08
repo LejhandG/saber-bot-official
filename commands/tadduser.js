8098b89a42bebdd4b3669c338511ada0e4e18170
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 10:27:19 +0530

    Update tadduser.js

--- commands/tadduser.js
+++ commands/tadduser.js
@@ -7,6 +7,7 @@ const ticket = new DiscordTicket()
 
 module.exports = {
     name: "tadduser",
+    timeout : 3000,
 description: "Adds user to ticket",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
