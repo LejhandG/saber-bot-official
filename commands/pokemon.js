c806a50a33faad0daacd6360f9677bc52026d72c
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 10:08:18 +0530

    Update pokemon.js

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require('discord.js')
 
 module.exports = {
     name: "pokemon",
+    timeout : 3000,
 description: "Finds Pokemon",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
