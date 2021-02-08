ce37c69015923857541afb5620b060928df79348
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 10:17:59 +0530

    Update shoot.js

--- commands/shoot.js
+++ commands/shoot.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     name: "shoot",
+    timeout : 3000,
 description: "Shoot Command",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
