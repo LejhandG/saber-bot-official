a5fd0bd0b316545765f700cef12b23b0327994cf
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 09:48:57 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   name: "ban",
+  timeout : 10000,
   description: "Ban user",
   alias: [],
   run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
