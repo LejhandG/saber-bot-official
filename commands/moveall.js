778146479a6e26b26432a764329195be0b12476e
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 10:05:30 +0530

    Update moveall.js

--- commands/moveall.js
+++ commands/moveall.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     name: "moveall",
+    timeout : 5000,
 description: "Moves everyone to the vc",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
