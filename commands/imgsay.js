4b3365feba6d80df7ac02e8fcfe39af406220c86
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 10:00:22 +0530

    Update imgsay.js

--- commands/imgsay.js
+++ commands/imgsay.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require('discord.js')
 
 module.exports = {
     name: "imgsay",
+    timeout : 3000,
 description: "Image Text",
 alias: ["imagesay"],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
