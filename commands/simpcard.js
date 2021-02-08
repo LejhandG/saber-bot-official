47aa2d5f6a5be4abeb9015bade57399cf0391d26
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 10:18:18 +0530

    Update simpcard.js

--- commands/simpcard.js
+++ commands/simpcard.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     name: "simpcard",
+    timeout : 3000,
 description: "Simpcard Command",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
