c0fec9e816c12bfd080034b003ee06bbcc001d6d
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 09:56:06 +0530

    Update createvoice.js

--- commands/createvoice.js
+++ commands/createvoice.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: "createvoice",
+    timeout : 5000,
 description: "Creates voice channel",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
