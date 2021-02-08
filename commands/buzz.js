294ef4799acfe9673cfec8fcbd46ee584b6c781f
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 09:50:42 +0530

    Update buzz.js

--- commands/buzz.js
+++ commands/buzz.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     name: "buzz",
+    timeout : 3000,
 description: "Buzz Command",
 alias: ["bzz", "bz"],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
