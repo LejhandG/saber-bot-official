6b51b2ce96751940365026dc59b1bf1daec65f6c
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 10:13:18 +0530

    Update roles.js

--- commands/roles.js
+++ commands/roles.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: "roles",
+    timeout : 3000,
 description: "Shows all the server roles",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
