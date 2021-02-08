fb21b8009f7b3bca8b68c5ed8808e837f90bd638
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 10:01:00 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = {
   name: "invite",
+  timeout : 3000,
   description: "Show bot invite url",
   alias: [],
   run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
