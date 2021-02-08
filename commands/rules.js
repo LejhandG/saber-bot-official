a01f98432e564dac6eeedb878a0479baa05179a4
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-08 10:14:02 +0530

    Update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
   name: "rules",
+	timeout : 30000,
   description: "Embeds rules",
   alias: [],
   run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
