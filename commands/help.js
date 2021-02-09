f1901aa916319a5b42975ddbe46f7ec7b15b7791
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-09 13:01:53 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -92,7 +92,7 @@ Prefix - \`${bot.prefix}\`
     .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
     .setDescription(`
     **__Rank Commands__**
-\`rank\`, \`leaderboard\`
+\`rank\`
     `)
     .setFooter("©️ 2021 Saber Bot");
 
