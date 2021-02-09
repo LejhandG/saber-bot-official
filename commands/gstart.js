8052f9287f01b3411cca9aa3227d2cb26cd26997
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-09 18:19:42 +0530

    Update gstart.js

--- commands/gstart.js
+++ commands/gstart.js
@@ -25,7 +25,7 @@ ${bot.prefix}gstart #giveaways 1d 1 Nitro
   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You don't have premission to do that!");
     
     let priz = args.slice(4).join(" ")
-    let time = ms(args[2])
+    let time = args[2]
     let winerz = args[3]
   
   const channel = message.mentions.channels.first();
@@ -39,7 +39,7 @@ ${bot.prefix}gstart #giveaways 1d 1 Nitro
             prize: priz,
             channelId: channel.id,
             guildId: message.guild.id,
-            duration: time, // 30 Seconds
+            duration: ms(time), // 30 Seconds
             winners: winerz, // 1 winner
             hostedBy: message.author.id
         });
