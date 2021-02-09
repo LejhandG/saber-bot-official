d2af4d15a1d2ebb213ffa62421cf69a12d34a77c
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-09 14:25:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -65,18 +65,6 @@ bot.on("message", async message => {
   let command = message.content.toLowerCase().split(" ")[0];
   command = command.slice(PREFIX.length);
   
-  if(db.has(`afk-${message.author.id}+${message.guild.id}`)) {
-        const info = db.get(`afk-${message.author.id}+${message.guild.id}`)
-        await db.delete(`afk-${message.author.id}+${message.guild.id}`)
-        message.reply(`Your afk status have been removed (${info})`)
-    }
-    //checking for mentions
-    if(message.mentions.members.first()) {
-        if(db.has(`afk-${message.mentions.members.first().id}+${message.guild.id}`)) {
-            message.channel.send(message.mentions.members.first().user.tag + ":" + db.get(`afk-${message.mentions.members.first().id}+${message.guild.id}`))
-        }else return;
-    }else;
-  
   const randomXp = Math.floor(Math.random() *9) + 1;
   const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
   if (hasLeveledUp) {
