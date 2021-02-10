b4aaae4c1f91901c51d34fc42d99bdfa9bc14096
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-10 11:02:12 +0530

    Create gend.js

---
+++ commands/gend.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: "gend",
+  timeout : 5000,
+  description: "Ends the giveaway",
+  alias: [],
+  run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+    
+    let wrong = new MessageEmbed()
+        .setTitle(`Command: ${bot.prefix}gend`)
+        .setDescription(`
+**Description:** 
+Ends the giveaway
+**Usage:**
+${bot.prefix}gend [message id]
+**Example:**
+${bot.prefix}gend 52511115343433
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
+    
+  if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You don't have permission to do that!");
+  
+        const ended = await client.giveaways.endGiveaway(args[1]);
+        
+        if (!ended) {
+            return message.channel.send('This giveaway has already ended');
+        }
+        else {
+            message.channel.send('Ended the giveaway');
+        }
+  }
+}
