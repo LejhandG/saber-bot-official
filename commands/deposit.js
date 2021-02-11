2b7a873c5131cabde98550ad3c48f6e6003af805
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-11 15:13:44 +0530

    Create deposit.js

---
+++ commands/deposit.js
@@ -0,0 +1,68 @@
+const { MessageEmbed } = require("discord.js")
+const db = require("quick.db")
+const Discord = require("discord.js")
+const ms = require("parse-ms")
+
+module.exports = {
+    name: "deposit",
+    timeout : 1,
+    description: "Deposit money into bank",
+    alias: ["dep"],
+    run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+        let user = message.author;
+
+  let member = db.fetch(`money_${message.guild.id}_${user.id}`)
+  let member2 = db.fetch(`bank_${message.guild.id}_${user.id}`)
+
+  if (args[1] == 'all') {
+    let money = await db.fetch(`money_${message.guild.id}_${user.id}`)
+    let bank = await db.fetch(`bank_${message.guild.id}_${user.id}`)
+
+    let embedbank = new Discord.MessageEmbed()
+    .setColor('#FFFFFF')
+    .setDescription("You don't have any money to deposit")
+
+    if(money === 0) return message.channel.send(embedbank)
+
+    db.add(`bank_${message.guild.id}_${user.id}`, money)
+    db.subtract(`money_${message.guild.id}_${user.id}`, money)
+    let embed5 = new Discord.MessageEmbed()
+  .setColor("#FFFFFF")
+  .setDescription(`You have deposited all your coins into your bank`);
+  message.channel.send(embed5)
+  
+  } else {
+  
+  let embed2 = new Discord.MessageEmbed()
+  .setColor("#FFFFFF")
+  .setDescription(`Specify an amount to deposit`);
+  
+  if (!args[1]) {
+      return message.channel.send(embed2)
+      .catch(err => console.log(err))
+  }
+  let embed3 = new Discord.MessageEmbed()
+  .setColor("#FFFFFF")
+  .setDescription(`You can't deposit negative money`);
+
+  if (message.content.includes('-')) { 
+      return message.channel.send(embed3)
+  }
+  let embed4 = new Discord.MessageEmbed()
+  .setColor("#FFFFFF")
+  .setDescription(`You don't have that much money`);
+
+  if (member < args[1]) {
+      return message.channel.send(embed4)
+  }
+
+  let embed5 = new Discord.MessageEmbed()
+  .setColor("#FFFFFF")
+  .setDescription(`You have deposited ${args[1]} coins into your bank`);
+
+  message.channel.send(embed5)
+  db.add(`bank_${message.guild.id}_${user.id}`, args[1])
+  db.subtract(`money_${message.guild.id}_${user.id}`, args[1])
+  }
+}
+}
