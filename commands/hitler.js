1218b6e2231f3800a52c290d1ee3f5a2d1f0f950
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-13 15:47:37 +0530

    Create hitler.js

---
+++ commands/hitler.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "hitler",
+    timeout : 3000,
+description: "Hitler Command",
+alias: [],
+run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+  
+    let user = message.mentions.users.first()
+    if (!user) user = message.author
+
+    let avatar = user.displayAvatarURL({ format: 'png' });
+
+  const helpembed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Hitler Command")
+      .setImage("https://api.devs-hub.xyz/hitler?image=" + avatar)
+      .setFooter(message.author.tag, message.author.avatarURL())
+    message.channel.send(helpembed).catch(err => {
+            message.reply("An error occured");
+          });
+}
+}
