174555f3b4cfd48431a910525b7d097f7c28b45f
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-13 17:20:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ bot.on("message", async message => {
   const PREFIX = db.get(`guild_${message.guild.id}_prefix`) || "/"
   bot.prefix = PREFIX;
   if (message.content.startsWith(`<@!${bot.user.id}>`) || message.content.startsWith(`<@${bot.user.id}>`)) {
-    PREFIX = `<@!${bot.user.id}> `;
+    const PREFIX = `<@!${bot.user.id}> `;
   }
   // eslint-disable-line
   if (message.author.bot) return;
