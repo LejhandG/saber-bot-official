78f981416908e563964470cedd219503198da923
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-15 12:03:40 +0530

    Create glitch.js

---
+++ commands/glitch.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "glitch",
+    timeout : 3000,
+description: "Glitch Command",
+alias: [],
+run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+  
+    let user = message.mentions.users.first()
+    if (!user) user = message.author
+
+    let avatar = user.displayAvatarURL({ format: 'png' });
+
+  const helpembed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Glitch Command")
+      .setImage("https://api.devs-hub.xyz/glitch?image=" + avatar)
+      .setFooter(message.author.tag, message.author.avatarURL())
+    message.channel.send(helpembed).catch(err => {
+            message.reply("An error occured");
+          });
+}
+}
