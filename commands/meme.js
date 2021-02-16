2e8d21b8db6dd4170823119a4973f77c342a7590
Dhruv Bhanushali <62509578+LejhandGamingYT@users.noreply.github.com>
2021-02-16 11:36:48 +0530

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -8,44 +8,15 @@ module.exports = {
 description: "Meme Commands",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
-    const subReddits = [
-    "meme",
-    "me_irl",
-    "dankmeme",
-    "AdviceAnimals",
-    "dankmemes",
-    "MemeEconomy",
-    "ComedyCemetery",
-    "memes",
-    "PrequelMemes",
-    "terriblefacebookmemes",
-    "funny",
-    "teenagers",
-    "comedyheaven",
-    "HistoryMemes",
-  ];
-  const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-
-  const embed = new Discord.MessageEmbed();
-  got(`https://www.reddit.com/r/${random}/random/.json`).then((response) => {
-    let content = JSON.parse(response.body);
-    let permalink = content[0].data.children[0].data.permalink;
-    let memeUrl = `https://reddit.com${permalink}`;
-    let memeImage = content[0].data.children[0].data.url;
-    let memeTitle = content[0].data.children[0].data.title;
-    let memeUpvotes = content[0].data.children[0].data.ups;
-    let memeDownvotes = content[0].data.children[0].data.downs;
-    let memeNumComments = content[0].data.children[0].data.num_comments;
-    embed.setTitle(`${memeTitle}`);
-    embed.setURL(`${memeUrl}`);
-    embed.setImage(memeImage);
-    embed.setColor("#ffc256")
-    embed.setFooter(
-      `👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`
-    );
-    message.channel.send(embed).catch(err => {
-            message.reply("An error occurred");
-          });
-   });
+  got('https://www.reddit.com/r/memes/random/.json').then(res => {
+            let content = JSON.parse(res.body)
+            message.channel.send(
+                new MessageEmbed()
+                    .setTitle(content[0].data.children[0].data.title)
+                    .setImage(content[0].data.children[0].data.url)
+                    .setColor("RANDOM")
+                    .setFooter(`👍 ${content[0].data.children[0].data.ups} 👎 ${content[0].data.children[0].data.downs} | Comments : ${content[0].data.children[0].data.num_comments}`)
+            )
+        })
 }
 }
