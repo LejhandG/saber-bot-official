7429129956b59213de23c43d3c0135d865ccf1cc
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2021-02-20 14:17:54 +0530

    Update clearwarns.js

--- commands/clearwarns.js
+++ commands/clearwarns.js
@@ -1,51 +1,22 @@
-const Discord = require('discord.js');
-const fs = require("fs");
-const db = require('quick.db')
-const { MessageEmbed } = require('discord.js')
+const db = require('../models/warns')
 
 module.exports = {
     name: "clearwarns",
     timeout : 5000,
-description: "Clears Warns of the user",
+description: "Clears the warns of the user",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
-
-    let wrong = new MessageEmbed()
-        .setTitle(`Command: ${bot.prefix}clearwarns`)
-        .setDescription(`
-**Description:** 
-Clears the warnings of the mentioned user
-**Usage:**
-${bot.prefix}clearwarns @user
-**Example:**
-${bot.prefix}clearwarns @Vortex
-`)
-        .setFooter(message.author.tag, message.author.avatarURL())
-        .setColor(`RANDOM`);
-
-    if(!message.member.hasPermission("MANAGE_ROLES")) {
-        return message.channel.send("You do not have permission to use this command")
-      }
-      
-      const user = message.mentions.members.first()
-      
-      if(!user) {
-      return message.channel.send(wrong)
-      }
-      
-      if(message.mentions.users.first().bot) {
-        return message.channel.send("Bot are not allowed to have warnings")
-      }
-      if(message.author.id === user.id) {
-        return message.channel.send("You are not allowed to reset your warnings")
-      }
-      
-      let warnings = db.get(`warnings_${message.guild.id}_${user.id}`)
-      if(warnings === null) {
-        return message.channel.send(`${message.mentions.users.first().username} do not have any warnings`)
-      }
-      db.delete(`warnings_${message.guild.id}_${user.id}`)
-      user.send(`Your all warnings are reseted by ${message.author.username} from ${message.guild.name}`)
-      await message.channel.send(`Reseted all warnings of **${message.mentions.users.first().username}**`) //DO NOT FORGET TO USE ASYNC FUNCTION    
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You do not have permission to use this command.')
+        const user = message.mentions.members.first() || message.guild.members.cache.get(args[1]);
+        if(!user) return message.channel.send('User not found.')
+        db.findOne({ guildid : message.guild.id, user: user.user.id}, async(err,data) => {
+            if(err) throw err;
+            if(data) {
+                await db.findOneAndDelete({ user : user.user.id, guildid: message.guild.id})
+                message.channel.send(`Cleared ${user.user.tag}'s warns`)
+            } else {
+                message.channel.send('This user does not have any warns in this server!')
+            }
+        })
 }
 }
