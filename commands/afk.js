bd4b2e6ed1c6d1a99ed25913f01eef7fe88401d4
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2021-02-23 12:16:04 +0530

    Create afk.js

---
+++ commands/afk.js
@@ -0,0 +1,17 @@
+const dbb = require("../reconDB")
+const { MessageEmbed } = require('discord.js')
+module.exports = {
+    name: "afk",
+    timeout : 5000,
+    description: "Show bot's ping",
+    alias: [],
+    run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
+        const content = args.slice(1).join(" ");
+        await dbb.set(`afk-${message.author.id}+${message.guild.id}`, content)
+        const embed = new MessageEmbed()
+        .setDescription(`You have been set to afk\n**Reason :** ${content}`)
+        .setColor("GREEN")
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
+        message.channel.send(embed)
+    }
+  }
