f6ede62393da78059a1add7c22bb9d465e92a015
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2021-02-24 10:58:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -97,19 +97,19 @@ if(message.mentions.members.first()) {
 }else;
   
   dbl.hasVoted(user.id).then(voted => {
-    if (voted) {
+    if (async voted) {
       const randomXp = Math.floor(Math.random() *10) + 1;
   const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
-  if (hasLeveledUp) {
+  if (async hasLeveledUp) {
     const user = await Levels.fetch(message.author.id, message.guild.id)
     message.channel.send(`You leveled up to ${user.level}! Keep it going!`)
   }
     }
     
-    else {
+    else async {
       const randomXp = Math.floor(Math.random() *9) + 1;
   const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
-  if (hasLeveledUp) {
+  if (async hasLeveledUp) {
     const user = await Levels.fetch(message.author.id, message.guild.id)
     message.channel.send(`You leveled up to ${user.level}! Keep it going!`)
   }
