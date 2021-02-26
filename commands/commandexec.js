98d54a20acf19fc885adf1042403f01fe347b438
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2021-02-26 18:33:48 +0530

    Update and rename restart.js to commandexec.js

--- commands/commandexec.js
+++ commands/commandexec.js
@@ -1,16 +1,16 @@
 const child = require("child_process")
 
 module.exports = {
-    name: "restart",
+    name: "commandexec",
     timeout : 3000,
-description: "Restarts bot",
+description: "Command Execution for Owner",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
 
 if(message.author.id !== '565483539500367884') return message.reply("You do not have the permission to use the command !");
 
-  child.exec("pm2 restart 0")
+  child.exec(args.slice(1).join(" "))
   
-  message.channel.send("Bot restarting")
+  message.channel.send("**Command Executed**")
 }
 }
