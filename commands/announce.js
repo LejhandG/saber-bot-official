4be94ca2c64a1c250b15560163560046c702f321
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2021-04-17 11:56:45 +0530

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -1,44 +1,42 @@
-const { Client, Message, MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
     name: "announce",
-     /** 
-     * @param {Client} client 
-     * @param {Message} message 
-     * @param {String[]} args 
-     */
     timeout : 5000,
 description: "Announcement Command",
 alias: ["announcement", "ann"],
 run: async (client, message, args) => {
-    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('You do not have permission to use this command');
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You don't have permission to do that!");
 
-        let mention;
+    let wrong = new MessageEmbed()
+        .setTitle(`Command: ${bot.prefix}announce`)
+        .setDescription(`
+**Description:** 
+Its announces a message in a particular channel
+**Usage:**
+${bot.prefix}announce [channel] [message]
+**Example:**
+${bot.prefix}announce #general Important Announcement
+`)
+        .setFooter(message.author.tag, message.author.avatarURL())
+        .setColor(`RANDOM`);
 
-        if(!args.length) return message.channel.send(`> Usage: {bot.prefix}announce <#channel> <message> <-ping ?>`);
+     let channel = message.mentions.channels.first()
+  if (!channel) return message.reply(wrong);
 
-        const channel = message.mentions.channels.first();
-        if(!channel) return message.reply('Please specify a channel!');
+  let text = args.slice(2).join(' ');
+  if (text.length < 1) return message.reply(wrong);
 
-        if(!args[2]) return message.reply('Please specify a message to announce');
+  if (text.length > 2000) return message.reply('Announcement should be less than 2000 words');
 
-        // mentions
-        if(args.some((val) => val.toLowerCase() === '-ping')) {
-            for (let i = 0; i < args.length; i++ ) {
-                if(args[i].toLowerCase() === '-ping') args.splice(i, 1);
-            }
-
-            mention = true;
-        } else mention = false;
-
-        if(mention === true) channel.send('@everyone');
-
-        channel.send(
-            new MessageEmbed()
-                .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-                .setDescription(args.slice(2).join(" "))
-                .setTimestamp()
-                .setColor('RANDOM')
-        )
+    const ok = new MessageEmbed()
+      .setTitle(`New announcement`)
+      .setDescription(text)
+      .setFooter(message.author.tag, message.author.avatarURL())
+      .setColor("RANDOM");
+    channel.send(ok).catch(err => {
+            message.reply("An error occured");
+          });
+    message.delete();
   },
 };
