d3ee801d31d914c4015fc908c4cf65a2044c8ce8
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2023-04-01 13:23:36 +0530

    Update reconDB.js

--- reconDB.js
+++ reconDB.js
@@ -1,7 +1,7 @@
 const { reconDB } = require('reconlx');
 
 const dbb = new reconDB({
-    uri: 'mongodb+srv://Lejhand:united60@saberofficial.ta9ju.mongodb.net/test'
+    uri: 'MONGO DB URL'
 })
 
 module.exports = dbb;
