4301b13194cff1a0f11a74808401c9b986c5054d
Dhruv Bhanushali <dhruvbhanushali5621@gmail.com>
2023-04-01 13:40:25 +0530

    Update commandexec.js

--- commands/commandexec.js
+++ commands/commandexec.js
@@ -7,7 +7,7 @@ description: "Command Execution for Owner",
 alias: [],
 run: async (bot, message, args, url, searchString, youtube, handleVideo, serverQueue, play) => {
 
-if(message.author.id !== '565483539500367884') return message.reply("You do not have the permission to use the command !");
+if(message.author.id !== 'YOUR ID') return message.reply("You do not have the permission to use the command !");
 
   child.exec(args.slice(1).join(" "))
   
